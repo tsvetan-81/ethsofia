baaedb084cc8910665adcb0fb80598d042a2ad7d
tsvetan-81 <31433015+tsvetan-81@users.noreply.github.com>
2024-10-19 21:45:59 +0300

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -7,33 +7,47 @@ contract Hackathon {
         string techStack;
         string description;
         address sponsor;
+        address[] teamMembers;
+        address mentor;
     }
 
     Challenge[] public challenges;
-    mapping(address => uint256) public mentorTokens;
+    mapping(address => uint256) public tokensEarned;
     mapping(address => string[]) public mentorNFTs;
 
-    // Event for when a challenge is created
     event ChallengeCreated(string title, string techStack, string description, address sponsor);
-    
-    // Event for when a mentor receives tokens and an NFT
-    event MentorRewarded(address mentor, uint256 tokens, string nftCV);
+    event CollaborationSuccess(address mentor, address[] team, uint256 tokensRewarded, string nftCV);
 
     // Sponsor creates a challenge
-    function createChallenge(string memory title, string memory techStack, string memory description) public {
-        challenges.push(Challenge(title, techStack, description, msg.sender));
+    function createChallenge(string memory title, string memory techStack, string memory description, address[] memory teamMembers, address mentor) public {
+        challenges.push(Challenge(title, techStack, description, msg.sender, teamMembers, mentor));
         emit ChallengeCreated(title, techStack, description, msg.sender);
     }
 
-    // Reward mentor with tokens and NFT
-    function rewardMentor(address mentor, uint256 tokens, string memory nftCV) public {
-        mentorTokens[mentor] += tokens;
-        mentorNFTs[mentor].push(nftCV);
-        emit MentorRewarded(mentor, tokens, nftCV);
+    // Reward mentor and team after successful collaboration
+    function rewardMentorAndTeam(uint256 challengeIndex, uint256 tokensForMentor, uint256 tokensForTeam, string memory nftCV) public {
+        require(challengeIndex < challenges.length, "Invalid challenge index");
+        Challenge storage challenge = challenges[challengeIndex];
+        
+        // Distribute tokens to mentor
+        tokensEarned[challenge.mentor] += tokensForMentor;
+        mentorNFTs[challenge.mentor].push(nftCV);
+
+        // Distribute tokens to each team member
+        for (uint i = 0; i < challenge.teamMembers.length; i++) {
+            tokensEarned[challenge.teamMembers[i]] += tokensForTeam;
+        }
+
+        emit CollaborationSuccess(challenge.mentor, challenge.teamMembers, tokensForMentor + (tokensForTeam * challenge.teamMembers.length), nftCV);
     }
 
     // Get the number of challenges
     function getChallengesCount() public view returns (uint256) {
         return challenges.length;
+    }
+
+    // Get total tokens earned by an address
+    function getTokensEarned(address user) public view returns (uint256) {
+        return tokensEarned[user];
     }
 }
